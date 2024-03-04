210ed437295f48703149717b1c11bda472e2e70d
Arianna <113852826+SalemWulf@users.noreply.github.com>
2024-03-04 09:04:39 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let image = document.getElementById("image")
+
+
+function mouseOver() {
+    image.innerHTML = ' <img class="projectImg" src="Photos/Bildschirmfoto 2024-02-25 um 12.14.30 PM.png" alt="">';
+}
+function mouseOut() {
+    image.innerHTML = '<img src="Photos/Hover.png" />';
+}
